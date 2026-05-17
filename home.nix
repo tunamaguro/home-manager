450f5331f01d01499e7ba94461a71448aebd1567
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-05-17 22:20:45 +0900

    direnv

--- home.nix
+++ home.nix
@@ -83,6 +83,12 @@
     vimAlias = true; 
   };
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
