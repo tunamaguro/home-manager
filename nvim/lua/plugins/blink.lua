618e1d6aa5b763521f4a1e20910f628bde79b36d
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-05-18 22:55:48 +0900

    wip

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -14,13 +14,6 @@ blink.setup({
   },
 
   completion = {
-    list = {
-      selection = {
-        preselect = false,
-        auto_insert = false,
-      },
-    },
-
     documentation = {
       auto_show = true,
       auto_show_delay_ms = 500,
