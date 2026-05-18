63ed67e87f72f5168d06c6b23cb3063468d846a4
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-05-18 23:18:58 +0900

    Revert "Keep nvim-tree visible for editing buffers"
    This reverts commit 7564033bc5332b0ce9e1003f7ee0c4dea3c2f55a.

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,58 +1,5 @@
-local api = require("nvim-tree.api")
-
-local git_filetypes = {
-  git = true,
-  gitcommit = true,
-  gitrebase = true,
-  gitconfig = true,
-  diff = true,
-
-  -- Future-proofing if fugitive or diffview is added later.
-  fugitive = true,
-  fugitiveblame = true,
-  DiffviewFiles = true,
-  DiffviewFileHistory = true,
-}
-
-local ignored_buftypes = {
-  terminal = true,
-  quickfix = true,
-  prompt = true,
-  nofile = true,
-}
-
-local function is_git_context()
-  return git_filetypes[vim.bo.filetype] == true
-end
-
-local function should_open_tree()
-  if vim.bo.filetype == "NvimTree" then
-    return false
-  end
-
-  if is_git_context() then
-    return false
-  end
-
-  if ignored_buftypes[vim.bo.buftype] then
-    return false
-  end
-
-  return true
-end
-
-local function sync_tree_visibility()
-  if is_git_context() then
-    if api.tree.is_visible() then
-      api.tree.close()
-    end
-    return
-  end
-
-  if should_open_tree() and not api.tree.is_visible() then
-    api.tree.open({ focus = false })
-  end
-end
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
   sort = {
@@ -92,24 +39,8 @@ require("nvim-tree").setup({
   },
 })
 
-vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter", "TabEnter" }, {
-  group = vim.api.nvim_create_augroup("user-nvim-tree-auto-open", { clear = true }),
-  callback = function()
-    vim.schedule(sync_tree_visibility)
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("user-nvim-tree-quit", { clear = true }),
-  callback = function()
-    if vim.bo.filetype == "NvimTree" and #vim.api.nvim_list_wins() == 1 then
-      vim.cmd("quit")
-    end
-  end,
-})
-
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", {
-  desc = "Focus file explorer",
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {
+  desc = "Toggle file explorer",
   silent = true,
 })
 
