351a285d63e217647408b3b0f8ed3b40d8178fd6
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-05-18 21:46:37 +0900

    Add Telescope configuration

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      },
+    },
+  },
+
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
+})
+
+pcall(telescope.load_extension, "fzf")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+  desc = "Find files",
+})
+
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+  desc = "Live grep",
+})
+
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+  desc = "Find buffers",
+})
+
+vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {
+  desc = "Recent files",
+})
+
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {
+  desc = "Document symbols",
+})
+
+vim.keymap.set("n", "<leader>fS", builtin.lsp_dynamic_workspace_symbols, {
+  desc = "Workspace symbols",
+})
