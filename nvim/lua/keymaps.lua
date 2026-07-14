281a9aeede2fed3efe956131c0128453213231bf
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-07-15 00:08:55 +0900

    Add Neovim save and quit keymaps (#12)

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,3 +2,15 @@ vim.keymap.set("i", "jk", "<Esc>", {
   desc = "Exit insert mode",
   silent = true,
 })
+
+vim.keymap.set("n", "<leader>w", "<cmd>update<cr>", {
+  desc = "Save file",
+})
+
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", {
+  desc = "Quit window",
+})
+
+vim.keymap.set("n", "<leader>Q", "<cmd>qall<cr>", {
+  desc = "Quit Neovim",
+})
