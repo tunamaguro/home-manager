73f07eee60c982ae0fd2b9898133de6ce672e73b
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-09-12 22:37:49 +0900

    Clarify native completion keymap comment

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,7 +3,7 @@ vim.keymap.set("i", "jk", "<Esc>", {
   silent = true,
 })
 
--- Native completion keymaps.
+-- Keymaps for Neovim's native completion.
 vim.keymap.set("i", "<Tab>", function()
   if vim.fn.pumvisible() == 1 then
     return "<C-n>"
