5fb7ded102c77ede1a2afa4c47563e0b79480272
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-09-12 23:00:51 +0900

    Use Neovim default completion keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,38 +3,6 @@ vim.keymap.set("i", "jk", "<Esc>", {
   silent = true,
 })
 
--- Keymaps for Neovim's native completion.
-vim.keymap.set("i", "<Tab>", function()
-  if vim.fn.pumvisible() == 1 then
-    return "<C-n>"
-  end
-  return "<Tab>"
-end, {
-  expr = true,
-  desc = "Select next completion item",
-})
-
-vim.keymap.set("i", "<S-Tab>", function()
-  if vim.fn.pumvisible() == 1 then
-    return "<C-p>"
-  end
-  return "<S-Tab>"
-end, {
-  expr = true,
-  desc = "Select previous completion item",
-})
-
-vim.keymap.set("i", "<CR>", function()
-  local completion = vim.fn.complete_info({ "selected" })
-  if vim.fn.pumvisible() == 1 and completion.selected >= 0 then
-    return "<C-y>"
-  end
-  return "<CR>"
-end, {
-  expr = true,
-  desc = "Accept completion item",
-})
-
 vim.keymap.set("n", "<leader>w", "<cmd>update<cr>", {
   desc = "Save file",
 })
